d049ea20f21ad3086d2fc14bdf5b17c8f23c94e4
Oleksii Teleshev <93404755+Sarumanus@users.noreply.github.com>
2026-01-07 10:50:05 +0100

    Delete paint-app-raw/paint directory

--- paint-app-raw/paint/src/main/java/com/oleksii/paint/tools/Tool.java
+++
@@ -1,9 +0,0 @@
-package com.oleksii.paint.tools;
-
-import javafx.scene.canvas.GraphicsContext;
-
-public interface Tool {
-    void onPress(GraphicsContext gc, double x, double y);
-    void onDrag(GraphicsContext gc, double x, double y);
-    void onRelease(GraphicsContext gc, double x, double y);
-}
